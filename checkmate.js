ce1e6f71a1bcb76683a5bd2998bfb0796cfa0f95
annaharutyunyan2 <43216494+annaharutyunyan2@users.noreply.github.com>
2018-12-01 12:33:37 +0400

    Add files via upload

---
+++ checkmate.js
@@ -0,0 +1,54 @@
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext('2d');
+canvas.width = 700;
+canvas.height = 700;
+let array = [];
+const create = function(){
+  createCanvas(400,400);
+  array[array.length]=[
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0]
+  ];
+  console.table(array);
+  addChessPieces();
+  addChessPieces();
+  console.table(array);
+}
+
+const addChessPieces = function(){
+  let choices = [];
+  for(let i=0; i < array[0].length; i++){
+    for(let j=0; j < array[0].length; j++){
+      if(array[i][j]===0){
+        choices.push({
+          x: i,
+          y: j
+        });
+      }
+    }
+  }
+  if(choices.length > 0){
+    let place = random(choices);
+    let rand = random(10);
+    if(rand > 5){
+    array[place.x][place.y] = 2;
+  }
+    else{
+      array[place.x][place.y] = 4;
+    }
+  }
+  const draw = function(){
+    background(0);
+    let w = 100;
+    for(let i=0; i < array[0].length; i++){
+      for(let j=0; j < array[0].length; j++){
+        noFill();
+        strokeWeight(2);
+        stroke(0);
+        rect(i*w, j*w, w, w);
+      }
+    }
+  }
+}
